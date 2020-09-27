efeaf903c5425f5f4c4cc663a3bbeba2b3f1adaf
Sergii Kvartnyk <parashut131@gmail.com>
2020-09-27 13:15:09 +0300

    add receiver

---
+++ model_lib/src/main/java/com/practice/weathermodel/receiver/NetworkConnectionReceiver.java
@@ -0,0 +1,24 @@
+package com.practice.weathermodel.receiver;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+
+import io.reactivex.Observable;
+import io.reactivex.subjects.PublishSubject;
+
+public class NetworkConnectionReceiver extends BroadcastReceiver {
+    private final PublishSubject<Integer> connectivityChangeObservable = PublishSubject.create();
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        if(ConnectivityManager.CONNECTIVITY_ACTION.equals(intent.getAction())){
+            connectivityChangeObservable.onNext(1);
+        }
+    }
+
+    public Observable<Integer> getConnectivityChangeObservable() {
+        return connectivityChangeObservable;
+    }
+}
