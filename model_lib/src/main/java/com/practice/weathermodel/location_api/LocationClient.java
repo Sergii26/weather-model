213e7fe642e71cb51a41d99f431b9579efb7a861
Sergii Kvartnyk <parashut131@gmail.com>
2020-09-30 16:43:50 +0300

    add second way to retrieve location in LocationClient

--- model_lib/src/main/java/com/practice/weathermodel/location_api/LocationClient.java
+++ model_lib/src/main/java/com/practice/weathermodel/location_api/LocationClient.java
@@ -4,6 +4,11 @@ package com.practice.weathermodel.location_api;
 import android.annotation.SuppressLint;
 import android.content.Context;
 import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.os.Looper;
+import android.util.Log;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
@@ -15,7 +20,7 @@ import io.reactivex.Observable;
 import io.reactivex.subjects.AsyncSubject;
 
 public class LocationClient implements LocationSupplier {
-
+    private final LocationManager locationManager;
     private final FusedLocationProviderClient fusedLocationClient;
     private final AsyncSubject<Result<Location>> locationsSubject = AsyncSubject.create();
     private final OnFailureListener onFailureListener = new OnFailureListener() {
@@ -29,16 +34,41 @@ public class LocationClient implements LocationSupplier {
         @Override
         public void onSuccess(Location location) {
             if (location != null) {
-                locationsSubject.onNext(new Result<>(location));
+                getLastLocationByLocationManager();
             } else {
                 locationsSubject.onNext(new Result<Location>(new Exception("Location must not be null")));
+                locationsSubject.onComplete();
             }
+
+        }
+    };
+    private final LocationListener listener = new LocationListener() {
+        @Override
+        public void onLocationChanged(Location location) {
+            Log.d("LOGG", "onLocationChanged " + location);
+            locationsSubject.onNext(new Result<>(location));
             locationsSubject.onComplete();
+        }
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+            Log.d("LOGG", "onStatusChanged " + provider + ", sts " + status);
+        }
+
+        @Override
+        public void onProviderEnabled(String provider) {
+            Log.d("LOGG", "onProviderEnabled " + provider);
+        }
+
+        @Override
+        public void onProviderDisabled(String provider) {
+            Log.d("LOGG", "onProviderDisabled " + provider);
         }
     };
 
     public LocationClient(Context context) {
         fusedLocationClient = LocationServices.getFusedLocationProviderClient(context);
+        locationManager = (LocationManager) context.getSystemService(Context.LOCATION_SERVICE);
     }
 
     @SuppressLint("MissingPermission")
@@ -46,8 +76,18 @@ public class LocationClient implements LocationSupplier {
         fusedLocationClient.getLastLocation().addOnSuccessListener(onSuccessListener).addOnFailureListener(onFailureListener);
     }
 
+    @SuppressLint("MissingPermission")
+    private void getLastLocationByLocationManager() {
+        locationManager.requestSingleUpdate(LocationManager.GPS_PROVIDER, listener, Looper.myLooper());
+        locationManager.requestSingleUpdate(LocationManager.NETWORK_PROVIDER, listener, Looper.myLooper());
+        locationManager.requestSingleUpdate(LocationManager.PASSIVE_PROVIDER, listener, Looper.myLooper());
+    }
+
     public Observable<Result<Location>> getLastLocationObservable() {
         getLastLocation();
         return locationsSubject;
     }
+
+
+
 }
